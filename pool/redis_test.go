f59596eeaf01f7c8441f752fa2ceb3994698cd63
Behzad B. Mokhtari <b.mokhtari@alibaba.ir>
2020-05-09 12:33:34 +0430

    pool: added redigo pool generator

---
+++ pool/redis_test.go
@@ -0,0 +1,57 @@
+package pool_test
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "git.alibaba.ir/rd/zebel-the-sailor-bluto/pool"
+)
+
+var _ = Describe("Redis", func() {
+
+	// --------------------------------- global funcs
+
+	var getCorrectConfig = func() Config {
+		address := os.Getenv("REDIS_ADDRESS")
+		return Config{
+			Address:               address,
+			ConnectTimeoutSeconds: 10,
+			ReadTimeoutSeconds:    10,
+		}
+	}
+
+	var getWrongConfig = func() Config {
+		return Config{
+			Address:               "blahblah",
+			ConnectTimeoutSeconds: 10,
+			ReadTimeoutSeconds:    10,
+		}
+	}
+
+	// --------------------------------- tests
+
+	Describe("GetPool", func() {
+		It("should connect to the redis server with correct info", func() {
+			pool, err := GetPool(getCorrectConfig())
+
+			Expect(err).To(BeNil())
+			Expect(pool).To(Not(BeNil()))
+
+			err = pool.Close()
+			Expect(err).To(BeNil())
+		})
+
+		It("should not connect to the redis server with incorrect info", func() {
+			pool, err := GetPool(getWrongConfig())
+
+			Expect(err).To(BeNil())
+			Expect(pool).To(Not(BeNil()))
+
+			err = pool.Close()
+			Expect(err).To(BeNil())
+		})
+	})
+
+})
