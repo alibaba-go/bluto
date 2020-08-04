bd3cc92fd28f0f01570f51e5e17da188203b528a
Sepehr Rafiei <s.rafiei@alibaba.ir>
2020-08-04 11:35:45 +0430

    bluto: fix bluto comment

--- bluto/bluto.go
+++ bluto/bluto.go
@@ -5,12 +5,12 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-// Bluto create ready to use redis client
+// Bluto is a wrapper over redis pool
 type Bluto struct {
 	pool *redis.Pool
 }
 
-// New create new Bluto instance
+// New creates new Bluto instance
 func New(config Config) (*Bluto, error) {
 	pool, err := GetPool(config)
 	if err != nil {
@@ -20,14 +20,14 @@ func New(config Config) (*Bluto, error) {
 	return bl, nil
 }
 
-// Borrow start redis command
+// Borrow borrows a redis connection from pool
 func (bl *Bluto) Borrow() *commander.Commander {
 	conn := bl.pool.Get()
 	commander := commander.New(conn)
 	return commander
 }
 
-// Close close redis pool
+// Close closes redis pool
 func (bl *Bluto) Close() error {
 	return bl.pool.Close()
 }
